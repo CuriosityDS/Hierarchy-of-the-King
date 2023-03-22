16c13143c03441da58b2e8d5698ed97f7af22234
CuriosityDS <CuriosityDS@yandex.ru>
2023-03-22 22:07:36 +0300

    Welcome

---
+++ Parallelogramm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Hierarchy_of_the_King
+{
+    public class Parallelogramm:Rectangle
+    {
+        Point[] points = new Point[4];
+        double ABC, BCD, CDA, DAB;
+        double AB, BC, CD, DA, AC, BD;
+        double S, P;
+        Parallelogramm() : base() { SearchAngles(); SearchSides(); SearchSquad(); }
+        Parallelogramm(Point p1, Point p2, Point p3, Point p4) : base(p1, p2, p3, p4) { SearchAngles(); SearchSides(); SearchSquad(); }
+
+        public override void SearchSquad()
+        {
+            S = CD*(CDA * DA);
+        }
+        public override string ToString()
+        {
+            return $"Параллелограмм: {base.ToString()}";
+        }
+    }
+}
---
+++ Rhomb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hierarchy_of_the_King
+{
+    public class Rhomb:Rectangle
+    {
+        Point[] points = new Point[4];
+        double ABC, BCD, CDA, DAB;
+        double AB, BC, CD, DA, AC, BD;
+        double S, P;
+        public Rhomb() : base() { SearchAngles(); SearchSides(); SearchSquad(); }
+        public Rhomb(Point p1, Point p2, Point p3, Point p4) : base(p1, p2, p3, p4) { SearchAngles(); SearchSides(); SearchSquad(); }
+        public override void SearchSquad()
+        {
+            S = CD * ((AC * BD) / (2 * CD));
+        }
+        public override string ToString()
+        {
+            return $"Ромбик: {base.ToString()}";
+        }
+    }
+}
